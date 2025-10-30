55e3ca7f8e8ded5c0840c85addda28884bb681d1
Andreia Machion <amachion@gmail.com>
2025-10-30 12:41:56 -0300

    apostila 09 até criação de usuário, página 54

--- backend.js
+++ backend.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require('bcrypt')
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -19,6 +20,13 @@ const Filme = mongoose.model ("Filme", mongoose.Schema({
   titulo: {type: String}, 
   sinopse: {type: String}
 }))
+
+const usuarioSchema = mongoose.Schema({
+  login: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
 
 //endpoint para atender um get filmes: http://localhost:3000/filmes
 app.get('/filmes', async (req, res) => {
@@ -38,6 +46,29 @@ app.post('/filmes', async (req, res) => {
     //buscar no banco a base atualizada
     const filmes = await Filme.find()
     res.json(filmes)
+})
+
+app.post('/signup', async (req, res) => {
+  try {
+    //captura as informações que o usuário digita
+    const login = req.body.login
+    const password = req.body.password
+    //criptografar a senha
+    const passwordCriptografada = await bcrypt.hash(password, 10)
+    //constroi um objeto da classe Usuario
+    const usuario = new Usuario({
+      login: login,
+      password: passwordCriptografada
+    })
+    //vamos salvar no banco, capturando a resposta do Mongo
+    const respostaMongo = await usuario.save()
+    console.log(respostaMongo)
+    res.status(201).end()
+  }
+  catch (exception) {
+    console.log(exception);
+    res.status(409).end()    
+  }
 })
 
 app.listen(3000, () => {
